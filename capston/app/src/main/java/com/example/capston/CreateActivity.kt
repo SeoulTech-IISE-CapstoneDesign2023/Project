99a4808a721a645181dfe2c358a870012e4b5dc0
unso99 <94777814+unso99@users.noreply.github.com>
2023-06-15 00:20:52 +0900

    ui 업데이트 & 날짜 설정 기능 구현

--- capston/app/src/main/java/com/example/capston/CreateActivity.kt
+++ capston/app/src/main/java/com/example/capston/CreateActivity.kt
@@ -1,20 +1,32 @@
 package com.example.capston
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.commit
 import com.example.capston.EditFragment.EditMappingFragment
 import com.example.capston.EditFragment.EditTodoFragment
 import com.example.capston.databinding.ActivityCreateBinding
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class CreateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateBinding
     private var startAddress = ""
     private var arrivalAddress = ""
+    private var dateString = ""
+    private var timeString = ""
+    private var startTime = ""
+    private var arrivalTime = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +63,87 @@ class CreateActivity : AppCompatActivity() {
                 }
             }
         }
+        binding.startDateValueTextView.setOnClickListener {
+            setDate(0)
+        }
+        binding.arriveDateValueTextView.setOnClickListener {
+            setDate(1)
+        }
+    }
+
+    private fun setDate(separator: Int) {
+        val calendar = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            dateString = "${year}/ ${month + 1} / ${dayOfMonth}"
+            if (separator == 0) {
+                binding.startDateValueTextView.text = "$dateString $timeString"
+                startTime = "$dateString $timeString"
+            } else {
+                binding.arriveDateValueTextView.text = "$dateString $timeString"
+                arrivalTime = "$dateString $timeString"
+            }
+        }
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            timeString = "오전 ${hourOfDay}:${minute}"
+            if (hourOfDay > 12) {
+                timeString = "오후 ${hourOfDay - 12}:${minute}"
+            } else if (hourOfDay == 12) {
+                timeString = "오후 ${hourOfDay}:${minute}"
+            }
+            if (separator == 0) {
+                binding.startDateValueTextView.text = "$dateString $timeString"
+                startTime = "$dateString ${hourOfDay}:${minute}"
+            } else {
+                binding.arriveDateValueTextView.text = "$dateString $timeString"
+                arrivalTime = "$dateString ${hourOfDay}:${minute}"
+            }
+            // 출발시간이 도착시간보다 빨리 못하게 나중에 버튼누르면 안되게 해야함
+            val dateTimeFormat = SimpleDateFormat("yyyy/ M / dd HH:mm", Locale.KOREA)
+            try{
+                val startDate = dateTimeFormat.parse(startTime)
+                val arrivalDate = dateTimeFormat.parse(arrivalTime)
+                Log.d("time","$startDate $arrivalDate")
+                if (startDate != null) {
+                    if (startDate >= arrivalDate){
+                        Toast.makeText(this,"시작시간은 도착시간보다 늦을 수 없습니다",Toast.LENGTH_SHORT).show()
+                        binding.arriveDateValueTextView.text = "0000/00/00 오전 00:00"
+                        arrivalTime = ""
+                    }
+                }
+            }catch (e: ParseException){
+                e.printStackTrace()
+            }
+        }
+        TimePickerDialog(
+            this,
+            timeSetListener,
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        ).show()
+        DatePickerDialog(
+            this,
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+    private fun getDateString(calendar: Calendar): String {
+        return "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.DAY_OF_MONTH)}"
+    }
+
+    private fun getTimeString(calendar: Calendar): String {
+        val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        val isAfternoon = hourOfDay >= 12
+        val hourFormatted = if (hourOfDay > 12) hourOfDay - 12 else hourOfDay
+
+        return if (isAfternoon) {
+            "오후 $hourFormatted:$minute"
+        } else {
+            "오전 $hourFormatted:$minute"
+        }
     }
 
     private fun getData() {
@@ -59,7 +152,6 @@ class CreateActivity : AppCompatActivity() {
             arrivalAddress = getString("arrivalAddress", "").toString()
         }
     }
-
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
